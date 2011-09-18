7bf143ba217bba3c5787053b6f5f57af3b37e4f5
namatyage@gmail.com <root@tyage-desktop.(none)>
2011-09-18 14:29:43 +0900

    add block

--- public/js/client.js
+++ public/js/client.js
@@ -1,49 +1,116 @@
 $(function() {
-	var users = [];
-	
 	/*
-		canvas
+		definition
 	*/
-	var ctx = $('#world').get(0).getContext('2d');
-	var drawWorld = function() {
-		ctx.clearRect(0, 0, 500, 300);
-		users.forEach(function(user) {
-			if (!user) return true;
-			drawUser(user);
-		});
+	var World = {
+		width: 500,
+		height: 300,
+		draw: function() {
+			World.ctx.clearRect(0, 0, World.width, World.height);
+			World.users.forEach(function(user) {
+				if (!user) return true;
+				user.draw();
+			});
+			World.drawBlocks();
+		},
+		blocks: [
+			[0,0,1,1,1,1,1],
+			[0,0,1,1,0,0,1],
+			[0,0,1,1,0,0,1],
+			[0,0,0,0,0,0,1],
+			[0,0,0,0,0,0,1],
+			[1,1,1,1,1,1,1],
+		],
+		blockHeight: 50,
+		blockWidth: 50,
+		drawBlocks: function() {
+			World.blocks.forEach(function(blocks, y) {
+				blocks.forEach(function(block, x) {
+					if (!block) return true;
+					
+					World.ctx.fillRect(x * World.blockWidth, y * World.blockWidth, 
+						World.blockHeight, World.blockWidth);
+				})
+			});
+		},
+		users: [],
+		userHeight: 50,
+		userWidth: 50,
+		ctx: $('#world').get(0).getContext('2d')
 	};
-	var drawUser = function(user) {
-		ctx.fillRect(user.x, user.y, 50, 50);
+	
+	var User = function(param) {
+		this.id = param.id;
+		this.pos = {
+			x: param.x,
+			y: param.y
+		};
+		this.color = param.color;
+	};
+	User.prototype.move = function(x, y) {
+		var nextPos = {
+			x: x,
+			y: y
+		};
+
+		if (this.canMove(nextPos)) {
+			this.pos = nextPos;
+		}
+	};
+	User.prototype.canMove = function(pos) {
+		var minBlockPos = {
+			x: Math.floor(pos.x/World.blockWidth),
+			y: Math.floor(pos.y/World.blockHeight)
+		};
+		var maxBlockPos = {
+			x: Math.floor((pos.x + World.userWidth)/World.blockWidth),
+			y: Math.floor((pos.y + World.userHeight)/World.blockHeight)
+		};
+
+		for (var y=minBlockPos.y;y<=maxBlockPos.y;++y) {
+			for (var x=minBlockPos.x;x<=maxBlockPos.x;++x) {
+				if (World.blocks[y][x]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	};
+	User.prototype.draw = function() {
+		World.ctx.fillRect(this.pos.x, this.pos.y, World.userWidth, World.userHeight);
 	};
 	
 	/*
 		Socket.IO
 	*/
 	var socket = io.connect();
-	
+	socket.on('');
 	socket.on('users', function(userList) {
-		users = userList;
-		drawWorld();
+		userList.forEach(function(user) {
+			if (!user) return true;
+			
+			World.users[user.id] = new User(user);
+		});
+		World.draw();
 	});
 	socket.on('in', function(user) {
-		users[user.id] = user;
-		drawWorld();
+		World.users[user.id] = new User(user);
+		World.draw();
 	});
 	socket.on('out', function(id) {
-		delete users[id];
-		drawWorld();
+		delete World.users[id];
+		World.draw();
 	});
 
 	socket.on('move', function(id, x, y) {
-		users[id].x = x;
-		users[id].y = y;
-		drawWorld();
+		World.users[id].move(x, y);
+		World.draw();
 	});
 	
 	/*
 		User controll
 	*/
-	var margin = 5;
+	var margin = 2;
 	$(window).keydown(function(e) {
 		var key2move = {
 			37: [-1, 0],
